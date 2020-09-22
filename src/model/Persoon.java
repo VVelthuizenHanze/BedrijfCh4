ad27fc957aa3f0fc5d22ec26ebf511bb0f27337f
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2020-09-22 09:31:47 +0200

    start van 1.1

---
+++ src/model/Persoon.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
+ * <p>
+ * Uitwerking van opdracht
+ * <p>
+ * Doel:
+ */
+public class Persoon {
+    public static int aantalPersonen;
+
+    public int personeelsNummer;
+    public String naam;
+    public String woonplaats;
+    public double maandSalaris;
+}
