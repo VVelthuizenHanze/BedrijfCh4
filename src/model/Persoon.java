378d81229d58cfefee43cb63580df2591a3409ed
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2020-09-22 11:16:42 +0200

    Dagdeel 1

--- src/model/Persoon.java
+++ src/model/Persoon.java
@@ -2,16 +2,33 @@ package model;
 
 /**
  * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
- * <p>
- * Uitwerking van opdracht
- * <p>
- * Doel:
+ *
+ * Sla de gegevens van werknemers van ons bedrijf op
  */
 public class Persoon {
-    public static int aantalPersonen;
+    public static int aantalPersonen = 0;
 
     public int personeelsNummer;
     public String naam;
     public String woonplaats;
     public double maandSalaris;
+
+    public Persoon(String naam, String woonplaats, double maandSalaris) {
+        this.naam = naam;
+        this.woonplaats = woonplaats;
+        this.maandSalaris = maandSalaris;
+        this.personeelsNummer = ++aantalPersonen;
+    }
+
+    public Persoon(String naam) {
+        this(naam, "Onbekend", 0.0);
+    }
+
+    public Persoon() {
+        this("Onbekend");
+    }
+
+    public double berekenJaarInkomen() {
+        return 12 * maandSalaris;
+    }
 }
