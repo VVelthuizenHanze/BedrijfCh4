c19804d869644eab5d5bf638cad89e46c0eda86c
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2020-09-22 11:30:20 +0200

    Dagdeel 3 opdracht 3

---
+++ src/model/Afdeling.java
@@ -0,0 +1,20 @@
+package model;
+
+/**
+ * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
+ *
+ * Sla informatie op over de afdelingen van ons bedrijf
+ */
+public class Afdeling {
+    private String afdelingsNaam;
+    private String afdelingsPlaats;
+
+    public Afdeling(String afdelingsNaam, String afdelingsPlaats) {
+        this.afdelingsNaam = afdelingsNaam;
+        this.afdelingsPlaats = afdelingsPlaats;
+    }
+
+    public Afdeling() {
+        this("Onbekend", "Onbekend");
+    }
+}
